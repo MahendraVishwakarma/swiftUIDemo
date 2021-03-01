bb886e6c9d056f24e42645cd88c86f5bab8239be
Mahendra Vishwakarma <mahendra010895@gmail.com>
2021-03-01 22:05:31 +0530

    Initial Commit

---
+++ Mahendra/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mahendra
+//
+//  Created by Mahendra Vishwakarma on 01/03/21.
+//  Copyright © 2021 Mahendra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
